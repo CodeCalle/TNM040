abd8b35f315b1f2dcab4df70588d3019f321bc90
Carl-Johan Höglind <chd16002@student.mdu.se>
2023-03-08 23:16:15 +0100

    Blue lines added and code clean up

--- lab1/src/CountryInfo.js
+++ lab1/src/CountryInfo.js
@@ -1,18 +1,15 @@
 const CountryInfo = ({ country }) => {
   country.sort((a, b) => parseFloat(b.area) - parseFloat(a.area))
-
-  // const filteredCountry = country.filter((item) => item.name.common !== 'Antarctica')
-  // const firstFifteen = filteredCountry.slice(0, 15)
-
   const filteredFifteen = country.filter((item) => item.name.common !== 'Antarctica').slice(0, 15)
 
   return (
-    // style={{ backgroundColor: 'blue' }}
     <div className='country'>
-
       {filteredFifteen.map(item => {
         return (
-          <p key={item.cca3}><b>{item.name.common}</b> {item.area} km<sup>2</sup></p>
+          <div key={item.cca3}>
+            <p><b>{item.name.common}</b> {item.area} km<sup>2</sup></p>
+            <hr style={{ backgroundColor: '#205da2', height: '12px' }} />
+          </div>
         )
       })}
     </div>
