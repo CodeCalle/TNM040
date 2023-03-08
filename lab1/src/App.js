0e22f44cee08d67428a5f44625278180323ae1e7
Carl-Johan Höglind <chd16002@student.mdu.se>
2023-03-08 01:45:01 +0100

    Step 3: Create a component

--- lab1/src/App.js
+++ lab1/src/App.js
@@ -2,13 +2,12 @@ import './App.css'
 import CountryInfo from './CountryInfo'
 import countries from 'world-countries'
 
-// console.log(countries)
-function App() {
+function App () {
   return (
-    <div className="App">
-              <CountryInfo country={countries[0]}/>
+    <div className='App'>
+      <CountryInfo country={countries[0]} />
     </div>
-  );
+  )
 }
 
 export default App
--- lab1/src/CountryInfo.js
+++ lab1/src/CountryInfo.js
@@ -1,8 +1,11 @@
-const CountryInfo = ({country}) => {
-    console.log(country.name.common)
-    return (
-        <p>{country.name.common}</p>
-    )
+const CountryInfo = ({ country }) => {
+  console.log(country)
+  return (
+    // style={{ backgroundColor: 'blue' }}
+    <div className='country'>
+      <p><b>{country.name.common}</b> {country.area} km<sup>2</sup></p>
+    </div>
+  )
 }
 
 export default CountryInfo
