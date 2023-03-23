e5e6d468f32b877bf0dd038f626e51857b51cbe7
Carl-Johan Höglind <chd16002@student.mdu.se>
2023-03-23 15:58:13 +0100

    Added region and restructured the code a bit

--- lab1/src/components/CountryInfo.js
+++ lab1/src/components/CountryInfo.js
@@ -2,13 +2,13 @@ const CountryInfo = ({ country, detailed, largestCountryByArea }) => {
   return (
     <div key={country.cca3}>
       <p><b>{country.name.common}</b> {country.area} km<sup>2</sup></p>
-      <div className='bar' style={{ ...styles.bar, width: ((country.area * 100) / largestCountryByArea + '%') }} />
       {detailed === true && (
         <>
-          <p>{country.capital}</p>
-          <p />
+          <p>Capital: {country.capital}</p>
+          <p>Region: {country.region}</p>
         </>
       )}
+      <div className='bar' style={{ ...styles.bar, width: ((country.area * 100) / largestCountryByArea + '%') }} />
     </div>
   )
 }
