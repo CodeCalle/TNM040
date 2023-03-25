4ad66ac7ed50f5a5968f839edf6e83a91a087972
Carl-Johan Höglind <chd16002@student.mdu.se>
2023-03-26 00:43:25 +0100

    Testing buttons in components

--- lab2/src/App.js
+++ lab2/src/App.js
@@ -2,15 +2,13 @@ import React, { useState } from 'react'
 import './App.css'
 import CountryInfo from './components/CountryInfo'
 import countries from 'world-countries'
-
-// const i = 0
+import Button from './components/buttonsComp'
 
 function App () {
   countries.sort((a, b) => parseFloat(b.area) - parseFloat(a.area))
   const filteredFifteen = countries.filter((item) => item.name.common !== 'Antarctica').slice(0, 15)
 
   const [number, setNumber] = useState(0)
-
   const increment = (event) => {
     setNumber(number + 1)
   }
@@ -21,13 +19,10 @@ function App () {
   return (
     <div className='App'>
       <div className='country'>
-        <button onClick={increment} className='button'>
-          Add One!
-        </button>
-        <button onClick={decrement} className='button'>
-          Remove One!
-        </button>
+        <Button onClick={increment}>+ 1</Button>
+        <Button onClick={decrement}>-1</Button>
         <h1>Current number is: {number}</h1>
+
         <h1>This is the 15 biggest countries on Earth!</h1>
         {filteredFifteen.map((item, index) => {
           return (
