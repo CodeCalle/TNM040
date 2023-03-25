01373caf1581b4ed3af69fd1ccf18a3c5a669e35
Carl-Johan Höglind <chd16002@student.mdu.se>
2023-03-25 18:18:37 +0100

    Lab2 start

--- lab2/src/App.js
+++ lab2/src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import CountryInfo from './components/CountryInfo'
+import countries from 'world-countries'
 
-function App() {
+let i = 0
+
+function App () {
+  countries.sort((a, b) => parseFloat(b.area) - parseFloat(a.area))
+  const filteredFifteen = countries.filter((item) => item.name.common !== 'Antarctica').slice(0, 15)
+
+  const buttonTest = event => {
+    i++
+    console.log(i)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <div className='country'>
+        <button onClick={buttonTest} className='button'>
+          Test Me!
+        </button>
+        <h1>This is the 15 biggest countries on Earth!</h1>
+        {filteredFifteen.map((item, index) => {
+          return (
+            <CountryInfo country={item} key={item.id} detailed={index < 5} largestCountryByArea={filteredFifteen[0].area} />
+          )
+        })}
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
