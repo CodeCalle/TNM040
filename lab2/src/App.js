3df8e05c8fa0c461a61c3d5bd87f7d0ff2f53ad5
Carl-Johan Höglind <chd16002@student.mdu.se>
2023-03-25 17:47:32 +0100

    Lab2 start

--- lab2/src/App.js
+++ lab2/src/App.js
@@ -1,23 +1,25 @@
-import './App.css'
-import CountryInfo from './components/CountryInfo'
-import countries from 'world-countries'
+import logo from './logo.svg';
+import './App.css';
 
-function App () {
-  countries.sort((a, b) => parseFloat(b.area) - parseFloat(a.area))
-  const filteredFifteen = countries.filter((item) => item.name.common !== 'Antarctica').slice(0, 15)
-
+function App() {
   return (
-    <div className='App'>
-      <div className='country'>
-        <h1>This is the 15 biggest countries on Earth!</h1>
-        {filteredFifteen.map((item, index) => {
-          return (
-            <CountryInfo country={item} key={item.id} detailed={index < 5} largestCountryByArea={filteredFifteen[0].area} />
-          )
-        })}
-      </div>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
