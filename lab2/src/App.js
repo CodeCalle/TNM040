3e01702b72d51ed4087e33898b9012542a375d86
Carl-Johan Höglind <chd16002@student.mdu.se>
2023-03-25 20:46:28 +0100

    Testing State in React

--- lab2/src/App.js
+++ lab2/src/App.js
@@ -1,24 +1,33 @@
+import React, { useState } from 'react'
 import './App.css'
 import CountryInfo from './components/CountryInfo'
 import countries from 'world-countries'
 
-let i = 0
+// const i = 0
 
 function App () {
   countries.sort((a, b) => parseFloat(b.area) - parseFloat(a.area))
   const filteredFifteen = countries.filter((item) => item.name.common !== 'Antarctica').slice(0, 15)
 
-  const buttonTest = event => {
-    i++
-    console.log(i)
+  const [number, setNumber] = useState(0)
+
+  const increment = (event) => {
+    setNumber(number + 1)
+  }
+  const decrement = (event) => {
+    setNumber(number - 1)
   }
 
   return (
     <div className='App'>
       <div className='country'>
-        <button onClick={buttonTest} className='button'>
-          Test Me!
+        <button onClick={increment} className='button'>
+          Add One!
         </button>
+        <button onClick={decrement} className='button'>
+          Remove One!
+        </button>
+        <h1>Current number is: {number}</h1>
         <h1>This is the 15 biggest countries on Earth!</h1>
         {filteredFifteen.map((item, index) => {
           return (
