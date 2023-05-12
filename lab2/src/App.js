90f224d5ae3ad0286f330fadec9fdeff8ce8f8af
Carl-Johan Höglind <chd16002@student.mdu.se>
2023-05-12 17:31:39 +0200

    Before restructure with react routing

--- lab2/src/App.js
+++ lab2/src/App.js
@@ -11,26 +11,12 @@ function App () {
   const filteredFifteen = countries.filter((item) => item.name.common !== 'Antarctica').slice(0, 15)
   const [searchString, setSearchString] = useState('')
 
-  // ### --- For button --- ###
-  // const [number, setNumber] = useState(0)
-  // const increment = (event) => {
-  //   setNumber(number + 1)
-  // }
-  // const decrement = (event) => {
-  //   setNumber(number - 1)
-  // }
-
   const filteredWords = countries.filter((country) => MatchSearch(country.name.common, searchString)).slice(0, 5)
   console.log(filteredWords)
 
   return (
     <div className='App'>
       <div className='country'>
-        {/* ### --- For button --- ### */}
-        {/* <Button onClick={increment}>+ 1</Button>
-        <Button onClick={decrement}>-1</Button>
-        <h1>Current number is: {number}</h1> */}
-
         <h1>Search for a country</h1>
         <TextInput onChange={setSearchString} value={searchString} />
         <p>Text from searchbar: {searchString}</p>
