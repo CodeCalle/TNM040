3ca1e62bd681f38fb5fdd4a3cc8b5d711bf02709
Carl-Johan Höglind <chd16002@student.mdu.se>
2023-06-08 14:52:03 +0200

    NoPage update

--- lab2/src/components/NoPage.js
+++ lab2/src/components/NoPage.js
@@ -1,5 +1,15 @@
 const NoPage = () => {
-  return <h1>404</h1>
+  return (
+    <h1 className='NoPage' style={{ ...styles.NoPage }}>404</h1>
+  )
+}
+
+const styles = {
+  NoPage: {
+    color: 'red',
+    fontSize: '100px',
+    textAlign: 'center'
+  }
 }
 
 export default NoPage
